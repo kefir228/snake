ba03ec3d267bba3b4d03461db1156db051982af1
Ihor Karpov <igora75@gmail.com>
2024-02-08 00:18:49 +0200

    project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    {/* TODO: write your code here */}
+  <section className="section">
+    <div className="section__item--first">1</div>
+    <div className="section__item--second">2</div>
+    <div className="section__item--third">3</div>
+    <div className="section__item--fourth">4</div>
+    <div className="section__item--fifth">5</div>
+    <div className="section__item--sixth">6</div>
+  </section>
+  </div>;
+}
+
+export default App;
